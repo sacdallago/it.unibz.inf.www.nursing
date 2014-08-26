e492f4350b99588cfa280da51239314e476d710f
sacdallago <sacdallago@gmail.com>
2014-08-26 08:22:18 +0200

    Create Notifications.js

---
+++ nursing/client/lib/Notifications.js
@@ -0,0 +1,14 @@
+Notifications = {
+	warn : function(title,message){
+		console.log("Title: " + title + "\n message: " + message);
+	},
+	error : function(title,message){
+		console.log("Title: " + title + "\n message: " + message);
+	},
+	info : function(title,message){
+		console.log("Title: " + title + "\n message: " + message);
+	},
+	success : function(title,message){
+		console.log("Title: " + title + "\n message: " + message);
+	}
+};
